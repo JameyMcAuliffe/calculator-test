fd7ad3ef22e13f50db29e0d28840a94708bc6ef4
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-06-23 11:18:40 -0500

    all tests passed

---
+++ src/calculator.js
@@ -0,0 +1,30 @@
+
+//add function
+function add(num1, num2) {
+	return num1 + num2;
+};
+
+//subtract function
+function subtract(num1, num2) {
+	return num1 - num2;
+};
+
+//multiply function
+function multiply(num1, num2) {
+	return num1 * num2;
+};
+
+//divide function
+function divide(num1, num2) {
+	return num1 / num2;
+};
+
+//square function
+function square(num) {
+	return num * num;
+};
+
+//squareRoot function
+function squareRoot(num) {
+	return Math.sqrt(num);
+};	
